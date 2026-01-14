428b9b3aa80b118bc0eb6b9ce74476dcdef7b9e1
Your Name <thornrithy2004@gmail.com>
2026-01-14 15:19:18 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/apd_app_clone/login_page/apd_login_page.dart';
-import 'package:my_app/apd_app_clone/sign_up/apd_first_page.dart';
 import 'package:my_app/apd_app_clone/user_setting_page/apd_user_setting_page.dart';
 
 void main() {
@@ -9,7 +7,7 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: Center(
         child: Scaffold(
-          body: ApdLoginPage(),
+          body: ApdUserSettingPage(),
         ),
       ),
     ),
