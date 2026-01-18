2b23a2e2348be54bed4035f299433caca56a4c23
Your Name <thornrithy2004@gmail.com>
2026-01-18 19:14:00 +0700

    add_new_bottomsheet to popup on homepage

--- lib/apd_app_clone/apd_pop_user_setting_page.dart
+++ lib/apd_app_clone/apd_pop_user_setting_page.dart
@@ -7,315 +7,107 @@ class ApdPopUserSettingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //backgroundColor: Colors.grey,
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        child: Column(
-          children: [
-            Expanded(
-              flex: 6,
-              child: Container(
-                height: double.infinity,
-                width: double.infinity,
-              ),
+    return Container(
+      child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Center(
+          child: Image.asset(
+            "Images/menu_icon/arrow_classic.jpg",
+            fit: BoxFit.contain,
+            height: 35,
+            width: 80,
+          ),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.5,
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Color(0xFF37B3C9), Color(0xFF3891C7)],
             ),
-            Container(
-              alignment: Alignment.center,
-              child: Image.asset(
-                "Images/menu_icon/arrow_classic.jpg",
-                fit: BoxFit.contain,
-                height: 35,
-                width: 80,
-              ),
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
             ),
-            Expanded(
-              flex: 6,
-              child: Container(
-                clipBehavior: Clip.hardEdge,
-                height: double.infinity,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Color(0xFF37B3C9),
-                      Color(0xFF3891C7),
+          ),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              InkWell(
+                onTap: () {
+                  Navigator.push( context, MaterialPageRoute(
+                      builder: (context) => ApdUserSettingPage()),
+                  );
+                  },
+                child: _menuItem(
+                  icon: "Images/menu_icon/user_setting_icon.jpg",
+                  label: "User Setting",
+                ),
+              ),
+              _menuItem(icon: "Images/menu_icon/contact_us_icon.jpg", label: "Contact Us"),
+              _menuItem(icon: "Images/menu_icon/faq_icon.jpg", label: "FAQ"),
+              _menuItem(icon: "Images/menu_icon/term_condition_icon.jpg", label: "Terms & Conditions"),
+              _menuItem(icon: "Images/menu_icon/about_us_icon.jpg", label: "About Us"),
+              SizedBox(
+                width: 230,
+                height: 70,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.cyan.withOpacity(0.5),
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                  ),
+                  onPressed: () {
+                    Navigator.push( context, MaterialPageRoute(
+                        builder: (context) => ApdLoginPage()),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("Images/menu_icon/logout_icon.jpg",
+                          height: 30,
+                          width: 30,
+                          color: Colors.white,
+                      ),
+                      const SizedBox(width: 20),
+                      const Text(
+                          "Log out",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                          ),
+                      ),
                     ],
                   ),
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      blurRadius: 10,
-                      offset: Offset(0, -4),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 50, right: 60, top: 45),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 2,
-                              child: Image.asset(
-                                "Images/menu_icon/user_setting_icon.jpg",
-                                height: 25,
-                                width: 25,
-                              ),
-                          ),
-                          SizedBox(width: 20,),
-                          Expanded(
-                            flex: 10,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  "User Setting",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                                Material(
-                                  color: Colors.transparent,
-                                  child: InkWell(
-                                    borderRadius: BorderRadius.circular(45),
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => ApdUserSettingPage()),
-                                      );
-                                    },
-                                    child: Image.asset(
-                                      "Images/menu_icon/three_dot.jpg",
-                                      height: 35,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            )
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 50, right: 60, top: 20),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 2,
-                            child: Image.asset(
-                              "Images/menu_icon/contact_us_icon.jpg",
-                              height: 25,
-                              width: 25,
-                            ),
-                          ),
-                          SizedBox(width: 20,),
-                          Expanded(
-                              flex: 10,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "Contact us",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                  Image.asset(
-                                    "Images/menu_icon/three_dot.jpg",
-                                    height: 35,
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              )
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 50, right: 60, top: 20),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 2,
-                            child: Image.asset(
-                              "Images/menu_icon/faq_icon.jpg",
-                              height: 25,
-                              width: 25,
-                            ),
-                          ),
-                          SizedBox(width: 20,),
-                          Expanded(
-                              flex: 10,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "FAQ",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                  Material(
-                                    color: Colors.transparent,
-                                    child: InkWell(
-                                      borderRadius: BorderRadius.circular(45), // match icon shape
-                                      onTap: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(builder: (context) => ApdUserSettingPage()),
-                                        );
-                                      },
-                                      child: Image.asset(
-                                        "Images/menu_icon/three_dot.jpg",
-                                        height: 35,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              )
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 50, right: 60, top: 20),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 2,
-                            child: Image.asset(
-                              "Images/menu_icon/term_condition_icon.jpg",
-                              height: 30,
-                              width: 30,
-                            ),
-                          ),
-                          SizedBox(width: 20,),
-                          Expanded(
-                              flex: 10,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "Term & Conditions",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                  Image.asset(
-                                    "Images/menu_icon/three_dot.jpg",
-                                    height: 35,
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              )
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 50, right: 60, top: 20),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 2,
-                            child: Image.asset(
-                              "Images/menu_icon/about_us_icon.jpg",
-                              height: 25,
-                              width: 25,
-                            ),
-                          ),
-                          SizedBox(width: 20,),
-                          Expanded(
-                              flex: 10,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "About us",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                  Image.asset(
-                                    "Images/menu_icon/three_dot.jpg",
-                                    height: 35,
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              )
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 40,),
-                    Center(
-                      child: SizedBox(
-                        width: 230,
-                        height: 70,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.cyan.withOpacity(0.5),
-                            padding: EdgeInsets.symmetric(vertical: 14),
-                          ),
-                          onPressed: (){
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => ApdLoginPage()),
-                            );
-                          },
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                flex: 4,
-                                  child: Image.asset(
-                                    "Images/menu_icon/logout_icon.jpg",
-                                    height: 30,
-                                    width: 30,
-                                    color: Colors.white,
-                                  ),
-                              ),
-                              Expanded(
-                                flex: 9,
-                                child: Container(
-                                  margin: EdgeInsets.only(left: 20),
-                                  child: Text(
-                                    "Log out",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          )
-                        ),
-                      ),
-                    ),
-                  ],
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 30),
+            ],
+          ),
         ),
+      ],
+    ),
+    );
+  }
+
+  Widget _menuItem({required String icon, required String label}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+      child: Row(
+        children: [
+          Image.asset(icon, height: 25, width: 25),
+          const SizedBox(width: 20),
+          Expanded(
+            child: Text(label,
+                style: const TextStyle(color: Colors.white, fontSize: 20)),
+          ),
+          Image.asset("Images/menu_icon/three_dot.jpg",
+              height: 35, color: Colors.white),
+        ],
       ),
     );
   }
 }
+
