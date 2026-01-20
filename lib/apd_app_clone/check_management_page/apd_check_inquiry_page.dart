5639077d56aa787d0369095a9d434b4929c2f7e4
Your Name <thornrithy2004@gmail.com>
2026-01-20 19:22:04 +0700

    add_new_listview

--- lib/apd_app_clone/check_management_page/apd_check_inquiry_page.dart
+++ lib/apd_app_clone/check_management_page/apd_check_inquiry_page.dart
@@ -9,9 +9,22 @@ class ApdCheckInquiryPage extends StatefulWidget {
 }
 
 class _ApdCheckInquiryPageState extends State<ApdCheckInquiryPage> {
+  final List<Map<String, String>> checkItems = [
+    {"checkNo": "000010000101", "status": "NEW"},
+    {"checkNo": "000010000102", "status": "NEW"},
+    {"checkNo": "000010000103", "status": "NEW"},
+    {"checkNo": "000010000104", "status": "NEW"},
+    {"checkNo": "000010000105", "status": "NEW"},
+    {"checkNo": "000010000106", "status": "NEW"},
+    {"checkNo": "000010000107", "status": "NEW"},
+    {"checkNo": "000010000108", "status": "NEW"},
+    {"checkNo": "000010000109", "status": "NEW"},
+    {"checkNo": "000010000110", "status": "New"},
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFE3F2FD),
       body: Container(
         child: Padding(
           padding: EdgeInsets.zero,
@@ -69,6 +82,132 @@ class _ApdCheckInquiryPageState extends State<ApdCheckInquiryPage> {
                       ),
                     ),
                   ],
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 4,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                        size: 30,
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Search Check No. and Status",
+                            border: InputBorder.none,
+                            hintStyle: TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  itemCount: checkItems.length,
+                  itemBuilder: (context, index) {
+                    final item = checkItems[index];
+                    return Container(
+                      height: 80,
+                      width: double.infinity,
+                      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Image.asset(
+                              "Images/menu_icon/check_no_icon.jpg",
+                              height: 40,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 6,
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 5, right: 20),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    flex: 12,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("Check No.", style: TextStyle( fontSize: 20, color: Colors.blue)),
+                                        Text(item["checkNo"]!,
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                color: Colors.grey,
+                                            ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Flexible(
+                                    flex: 4,
+                                    child: Container(
+                                      alignment: Alignment.centerRight,
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text("Status", style: TextStyle( fontSize: 18, color: Colors.grey)),
+                                          Text(item["status"]!,
+                                              style: const TextStyle( fontSize: 20, color: Colors.blue),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Icon(
+                                Icons.keyboard_arrow_down,
+                                color: Colors.blue,
+                                size: 35,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                    },
                 ),
               ),
             ],
