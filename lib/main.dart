b365af3d5e7632cc6974c039a8d9a53837b4758a
Your Name <thornrithy2004@gmail.com>
2026-01-22 19:43:34 +0700

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
+import 'package:my_app/apd_app_clone/check_management_page/apd_request_check_page.dart';
 import 'package:my_app/apd_app_clone/home_page/apd_homepage_classic.dart';
 import 'package:my_app/apd_app_clone/login_page/apd_login_page.dart';
-import 'package:my_app/apd_app_clone/sign_up/apd_first_page.dart'; // language selection page
+import 'package:my_app/apd_app_clone/sign_up/apd_first_page.dart';
 import 'package:my_app/constants.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Hive.initFlutter();
-  final box = await Hive.openBox('userSavedBox');
-  final isLanguageSelected = box.get(AppConstants.isLanguageSelected, defaultValue: false);
-  final isLogin = box.get(AppConstants.isLogin, defaultValue: false);
 
+  final box = await Hive.openBox('userSavedBox');
+
+  // Read both flags
+  final bool isLanguageSelected =
+  box.get(AppConstants.isLanguageSelected, defaultValue: false);
+  final bool isLogin =
+  box.get(AppConstants.isLogin, defaultValue: false);
+  final bool isExisting =
+  box.get(AppConstants.isExisting, defaultValue: false);
+
+  print("isLanguageSelected: $isLanguageSelected, isLogin: $isLogin, isExisting: $isExisting");
+
+  // Decide start page
   Widget startPage;
   if (!isLanguageSelected) {
-    startPage = const FirstPage();
-  } else if (!isLogin) {
-    startPage = const ApdLoginPage();
+    startPage = FirstPage();
+  } else if (!isLogin && isExisting ) {
+    startPage = ApdLoginPage();
   } else {
-    startPage = const ApdHomePageClassic();
+    startPage = ApdHomePageClassic();
   }
 
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: startPage,
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: startPage
+    ),
+  );
 }
